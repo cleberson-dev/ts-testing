3d575c8c18f95fc643aff8aec4f2a4667291ff72
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-05 20:04:01 -0300

    Formatted Product class

--- Product.ts
+++ Product.ts
@@ -1,4 +1,4 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 export default class Product {
   private _id: string;
@@ -7,7 +7,11 @@ export default class Product {
   name: string;
   category: ProductCategories;
 
-  constructor(name: string, price: number, category: ProductCategories = ProductCategories.Other) {
+  constructor(
+    name: string,
+    price: number,
+    category: ProductCategories = ProductCategories.Other
+  ) {
     this._id = uuid();
     this.name = name;
     this._price = price;
@@ -23,12 +27,12 @@ export default class Product {
   }
 
   set price(newPrice: number) {
-    if (newPrice < 0) throw Error('');
+    if (newPrice < 0) throw Error("");
     this.price = newPrice;
   }
 
   get formattedPrice() {
-    return "R$" + this._price.toFixed(2).replace('.', ',');
+    return "R$" + this._price.toFixed(2).replace(".", ",");
   }
 }
 
@@ -36,5 +40,5 @@ enum ProductCategories {
   Videogame = "Videogame",
   Smartphone = "Smartphone",
   Furniture = "Móvel",
-  Other = 'Other'
+  Other = "Other",
 }
