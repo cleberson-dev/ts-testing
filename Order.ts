6e01b8ee7f63f2990d8e543262b57401363b4981
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-05 20:03:14 -0300

    Formatted Order class

--- Order.ts
+++ Order.ts
@@ -1,4 +1,4 @@
-import Product from "./Product"
+import Product from "./Product";
 import Warehouse from "./Warehouse";
 
 export default class Order {
@@ -6,39 +6,53 @@ export default class Order {
   private _isFilled: boolean = false;
 
   constructor(orderProducts: OrderProduct[]) {
-    if (orderProducts.some(product => product.amount <= 0)) throw Error('');
+    if (orderProducts.some((product) => product.amount <= 0)) {
+      throw Error("");
+    }
     this._orderProducts = orderProducts;
   }
 
   get products() {
-    return this._orderProducts.map(orderProduct => orderProduct.product);
+    return this._orderProducts
+      .map((orderProduct) => orderProduct.product);
   }
 
   addProduct(product: Product, amount: number) {
-    if (amount <= 0) throw Error('');
+    if (amount <= 0) throw Error("");
     this._orderProducts.push({ product, amount });
   }
 
   info() {
-    this._orderProducts.forEach(orderProduct => {
+    this._orderProducts.forEach((orderProduct) => {
       const { id, price } = orderProduct.product;
       const { amount } = orderProduct;
-      console.log(`#${id} | ${amount} unidades | R$${(price * amount).toFixed(2) }`);
+      const formattedCost = (price * amount).toFixed(2);
+      console.log(
+        `#${id} | ${amount} unidades | R$${formattedCost}`
+      );
     });
-    console.log('Preço total: R$' + this.cost.toFixed(2));
+    
+    console.log("Preço total: R$" + this.cost.toFixed(2));
   }
 
   get cost() {
-    return this._orderProducts.reduce((prev, acc) => prev + acc.product.price * acc.amount, 0);
+    return this._orderProducts.reduce(
+      (prev, acc) => prev + acc.product.price * acc.amount,
+      0
+    );
   }
 
   fill(warehouse: Warehouse) {
     // Precisa ter estoque para todo o pedido ter a retirada confirmada
-    if (this._orderProducts.some(op => !warehouse.hasInventory(op.product.id, op.amount))) {
-      throw Error('');
+    if (
+      this._orderProducts.some(
+        (op) => !warehouse.hasInventory(op.product.id, op.amount)
+      )
+    ) {
+      throw Error("");
     }
-    
-    this._orderProducts.forEach(orderProduct => {
+
+    this._orderProducts.forEach((orderProduct) => {
       warehouse.remove(orderProduct.product.id, orderProduct.amount);
     });
     this._isFilled = true;
