6fc2c671d0f9fe7a279fdff330d00bfdcce1bbdd
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-06 14:16:01 -0300

    Wrote Order test case 'it should have a total of 4 items'

--- __tests__/Order.spec.ts
+++ __tests__/Order.spec.ts
@@ -17,3 +17,14 @@ it("should have 3 products", () => {
 
   expect(order.products).toHaveLength(3);
 });
+
+it("should have total amount of 4", () => {
+  const orderProducts = [
+    { product: new ProductMock("Item 1", 10) as Product, amount: 1 },
+    { product: new ProductMock("Item 2", 20) as Product, amount: 2 },
+    { product: new ProductMock("Item 3", 30) as Product, amount: 1 },
+  ];
+  const order = new Order(orderProducts);
+
+  expect(order.totalAmount).toEqual(4);
+});
