db0514f9df3b8bf33fd83bc1dfcb7fa26f679509
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-06 14:24:19 -0300

    Wrote Order test case 'it should throw an error if it has some invalid products'

--- __tests__/Order.spec.ts
+++ __tests__/Order.spec.ts
@@ -97,3 +97,14 @@ it("should fill the order only if it hasn't been", () => {
 
   expect(mockHasInventory.mock.calls).toHaveLength(0);
 });
+
+it("should throw an error if it has some invalid products", () => {
+  const orderProducts = [
+    { product: new ProductMock("Item 1", 10) as Product, amount: 1 },
+    { product: new ProductMock("Item 2", 20) as Product, amount: 2 },
+    { product: new ProductMock("Item 3", 30) as Product, amount: -2 },
+  ];
+  expect(() => {
+    new Order(orderProducts);
+  }).toThrow();
+});
