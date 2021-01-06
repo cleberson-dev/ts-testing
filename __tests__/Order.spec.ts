8120f076155fd679c8b3a84e6ea48a9971b4de15
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-06 14:20:50 -0300

    Wrote Order test case 'it should fill the order'

--- __tests__/Order.spec.ts
+++ __tests__/Order.spec.ts
@@ -1,10 +1,25 @@
 import Order from "../src/Order";
 import Product from "../src/Product";
+import Warehouse from "../src/Warehouse";
 
 const ProductMock = jest.fn((name: string, price: number) => ({
   name,
   price,
 }));
+
+const mockHasInventory = jest.fn((productId: string, amount: number) => true);
+const mockRemove = jest.fn((productId: string, amountToRemove: number) => {});
+const WarehouseMock = jest.fn().mockImplementation(() => ({
+  _products: [],
+  hasInventory: mockHasInventory,
+  remove: mockRemove,
+  add: (productId: string, amount: number) => {},
+  info: () => {},
+  findProduct: (productId: string) => ({ id: "e3412s", amount: 1 }),
+  getAmount: (productId: string) => 1,
+}));
+
+
 
 it("should have 3 products", () => {
   const orderProducts = [
@@ -52,4 +67,17 @@ it("should add a product to the order", () => {
   expect(order.products).toHaveLength(4);
   expect(order.cost).toEqual(120);
   expect(order.totalAmount).toEqual(5);
+});
+
+it("should fill the order", () => {
+  const myMockWarehouse = new WarehouseMock();
+  const orderProducts = [
+    { product: new ProductMock("Item 1", 10) as Product, amount: 1 },
+    { product: new ProductMock("Item 2", 20) as Product, amount: 2 },
+    { product: new ProductMock("Item 3", 30) as Product, amount: 1 },
+  ];
+  const order = new Order(orderProducts);
+  order.fill(myMockWarehouse as Warehouse);
+
+  expect(order.isFilled).toEqual(true);
 });
