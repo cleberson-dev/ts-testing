78757a4e68c8df2db25518db92092afa16d94e59
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-06 14:18:56 -0300

    Wrote Order test case 'it should add a product to the order'

--- __tests__/Order.spec.ts
+++ __tests__/Order.spec.ts
@@ -39,3 +39,17 @@ it("should cost a total of $80", () => {
 
   expect(order.cost).toEqual(80);
 });
+
+it("should add a product to the order", () => {
+  const orderProducts = [
+    { product: new ProductMock("Item 1", 10) as Product, amount: 1 },
+    { product: new ProductMock("Item 2", 20) as Product, amount: 2 },
+    { product: new ProductMock("Item 3", 30) as Product, amount: 1 },
+  ];
+  const order = new Order(orderProducts);
+  order.addProduct(new ProductMock("Item 4", 40) as Product, 1);
+
+  expect(order.products).toHaveLength(4);
+  expect(order.cost).toEqual(120);
+  expect(order.totalAmount).toEqual(5);
+});
