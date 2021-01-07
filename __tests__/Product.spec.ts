69b9d047b498f41c5678b269c7d41f37c2828e51
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-07 01:03:29 -0300

    Wrote Product test case 'it should generate unique ids'

--- __tests__/Product.spec.ts
+++ __tests__/Product.spec.ts
@@ -6,3 +6,12 @@ it("should have the name 'Playstation 5' and a price of $599.99", () => {
   expect(product.name).toEqual("Playstation 5");
   expect(product.price).toEqual(599.99);
 });
+
+it("should generate unique ids", () => {
+  const productA = new Product("Item 1", 99.99);
+  const productB = new Product("Item 2", 99.99);
+
+  expect(productA.id).toBeTruthy();
+  expect(productB.id).toBeTruthy();
+  expect(productA.id).not.toEqual(productB.id);
+});
