6563160eb01bc5fc2bc7d479fa4d34244cf5d40c
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-07 00:51:32 -0300

    Wrote Warehouse test case 'it should have a total of 5 items'

--- __tests__/Warehouse.spec.ts
+++ __tests__/Warehouse.spec.ts
@@ -8,3 +8,12 @@ it("should have 3 products", () => {
   ]);
   expect(warehouse.products).toHaveLength(3);
 });
+
+it("should have a total of 5 items", () => {
+  const warehouse = new Warehouse([
+    { id: "1234", amount: 1 },
+    { id: "1235", amount: 1 },
+    { id: "1237", amount: 3 },
+  ]);
+  expect(warehouse.totalAmount).toEqual(5);
+});
